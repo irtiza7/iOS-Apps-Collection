5cd942cabea223a3ea4a9cc5962a1c7e6e985fc9
Muhammad Irtiza <m7.irtiza@gmail.com>
2024-07-23 00:36:02 +0500

    Initial commit. Created xcode project.

---
+++ Weather-SwiftUI/Weather-SwiftUI/Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwiftUIApp.swift
+//  Weather-SwiftUI
+//
+//  Created by Dev on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
